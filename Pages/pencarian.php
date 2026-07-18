6ac50f849ba5b9a84beab90fecc0a134283efcad
ginaprtwi <gpratiwi878@gmail.com>
2026-07-18 07:45:09 +0700

    nambah tampilan lg

---
+++ Pages/pencarian.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="../css/reset2.css" />
+	<link rel="stylesheet" href="../css/global2.css" />
+	<link rel="stylesheet" href="../css/pencarian.css" />
+
+</head>
+
+<body>
+<div class="pencarian">
+	<div class="pencarian-body-a">
+		<div class="pencarian-app">
+			<header class="header">
+				<div class="header-container">
+					<div class="container-a container3">
+						<img src="../assets/container/container-wire-box.png" class="wire-box-a container-wire-box" />
+						<p class="container-text1">Spotly.Kom</p>
+					</div>
+					
+					<div class="navigation nav">
+						<a href="beranda.php" class="text-button navigation-text-btn1" style="text-decoration: none;">Beranda</a>
+						<a href="pengelolaan.php" class="navigation-text-btn2" style="text-decoration: none;">Pengelolaan Tempat</a>
+						<p class="navigation-text-btn3">Profil</p>
+					</div>
+				</div>
+			</header>
+			
+			<div class="pencarian-container1">
+				<p class="pencarian-text1">Pencarian</p>
+				<p class="pencarian-text2">Ba</p>
+				<p class="pencarian-text3">Beranda</p>
+				<object data="../assets/detail-tempat-icon.svg" class="pencarian-icon1" type="image/svg+xml"></object>
+				<p class="pencarian-text4">Pencarian</p>
+			</div>
+			
+			<div class="pencarian-main-content">
+				<div class="pencarian-section">
+					<div class="pencarian-container2">
+						<div class="pencarian-container3">
+							<img src="../assets/pencarian/pencarian-tabloid1.png" class="pencarian-tabloid1" />
+							<h2 class="pencarian-subtitle subtitle-primary">Pencarian Tempat Belajar</h2>
+							<p class="pencarian-text-paragraph1 text1">
+								Temukan tempat belajar terbaik di sekitarmu<br />
+								dan belajar lebih nyaman hari ini.
+							</p>
+						</div>
+						
+						<img src="../assets/pencarian/pencarian-tabloid2.png" class="pencarian-tabloid2 input" />
+					</div>
+				</div>
+				
+				<div class="pencarian-container4">
+					<div class="pencarian-filter-sidebar">
+						<button class="btn1 hover-dark">
+							<object data="../assets/pencarian/btn-icon1.svg" class="btn-icon1" type="image/svg+xml"></object>
+							<p class="btn-label1">Cari tempat belajar...</p>
+						</button>
+						
+						<div class="check-group check-group1">
+							<p class="check-group-text">Kategori Tempat</p>
+							
+							<div class="check-group-container">
+								<div class="label-a label1">
+									<object data="../assets/pencarian/label/label-fluent-radio.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">Di Kampus</p>
+								</div>
+								
+								<div class="label-a label2">
+									<object data="../assets/pencarian/label/label-fluent-radio.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">Sekitar Kampus</p>
+								</div>
+								
+								<div class="label-b check-group-label">
+								</div>
+							</div>
+						</div>
+						
+						<div class="check-group check-group2">
+							<p class="check-group-text">Suasana</p>
+							
+							<div class="check-group-container">
+								<div class="label-a label1">
+									<object data="../assets/pencarian/label/label-fluent-radio.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">Tenang (Privat)</p>
+								</div>
+								
+								<div class="label-a label2">
+									<object data="../assets/pencarian/label/label-fluent-radio.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">Ramah diskusi </p>
+								</div>
+								
+								<div class="label-b check-group-label">
+								</div>
+							</div>
+						</div>
+						
+						<div class="label-b pencarian-container5">
+						</div>
+						
+						<div class="pencarian-check-group">
+							<p class="text3">Fasilitas</p>
+							
+							<div class="pencarian-container6">
+								<div class="label-a label3">
+									<object data="../assets/pencarian/label/label-carbon.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">Colokan</p>
+								</div>
+								
+								<div class="label-a label4">
+									<object data="../assets/pencarian/label/label-carbon.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">AC</p>
+								</div>
+								
+								<div class="label-a label5">
+									<object data="../assets/pencarian/label/label-carbon.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">WiFi</p>
+								</div>
+								
+								<div class="label-a label6">
+									<object data="../assets/pencarian/label/label-carbon.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">Mushola</p>
+								</div>
+								
+								<div class="label-a label7">
+									<object data="../assets/pencarian/label/label-carbon.svg" class="label-carbon-checkbox label-fluent-radio" type="image/svg+xml"></object>
+									<p class="label-text">Parkir</p>
+								</div>
+							</div>
+						</div>
+						
+						<div class="label-b pencarian-container7">
+						</div>
+						
+						<div class="pencarian-container8">
+							<p class="pencarian-text-paragraph3">Urutkan</p>
+							
+							<div class="pencarian-filter-select">
+								<p class="pencarian-text-rating-tertinggi">Rating Tertinggi</p>
+								<object data="../assets/pencarian/pencarian-icon.svg" class="pencarian-icon2" type="image/svg+xml"></object>
+							</div>
+						</div>
+						
+						<div class="pencarian-container9">
+							<button class="pencarian-btn text-white hover-bright">Terapkan Filter</button>
+							
+							<button class="btn2 hover-zoom">
+								<object data="../assets/pencarian/btn-icon2.svg" class="btn-icon2" type="image/svg+xml"></object>
+								<p class="btn-label2"> Reset</p>
+							</button>
+						</div>
+						
+						<div class="label-b pencarian-container10">
+						</div>
+					</div>
+					
+					<div class="pencarian-results-area">
+						<div class="pencarian-container11">
+							<a href="detail.php" class="card-place-a card-place1" style="text-decoration: none; color: inherit;">
+								<img src="../assets/pencarian/card-place/card-place-wire-box.png" class="wire-box-b card-place-wire-box1" />
+								
+								<div class="card-place-container1">
+									<p class="text-dark2">Perpustakaan Gasibu</p>
+									
+									<div class="container-e container1">
+										<object data="../assets/pencarian/container/container-icon.svg" class="container-icon" type="image/svg+xml"></object>
+										<p class="container-text5">4.3</p>
+									</div>
+									
+									<p class="card-place-text-paragraph1">Jl. Majapahit, Citarum, Bandung Wetan</p>
+									<button class="btn-margin card-place-btn-margin hover-dark">Colokan</button>
+								</div>
+							</a>
+							
+							<p class="pencarian-text-container"><b class="bold-primary">12 </b>Tempat ditemukan</p>
+							
+							<a href="detail.php" class="card-place-b card-place2" style="text-decoration: none; color: inherit;">
+								<img src="../assets/pencarian/card-place/card-place-wire-box2.png" class="wire-box-b card-place-wire-box2" />
+								
+								<div class="card-place-container2">
+									<p class="text-dark2">Eduplex Coworking Space</p>
+									
+									<div class="container-e container2">
+										<object data="../assets/pencarian/container/container-icon.svg" class="container-icon" type="image/svg+xml"></object>
+										<p class="container-text5">4.6</p>
+									</div>
+									
+									<p class="card-place-text-paragraph2">Jl. Ir. H. Djuanda No. 84, Bandung</p>
+									
+									<div class="card-place-container3">
+										<button class="btn-tag card-place-btn-tag1 hover-dark">Tenang</button>
+										<button class="btn-tag card-place-btn-tag2 hover-dark">WiFi</button>
+									</div>
+								</div>
+							</a>
+						</div>
+						
+						<div class="pencarian-container12">
+							<a href="detail.php" class="card-place-a card-place3" style="text-decoration: none; color: inherit;">
+								<img src="../assets/pencarian/card-place/card-place-wire-box.png" class="wire-box-b card-place-wire-box1" />
+								
+								<div class="card-place-container1">
+									<p class="text-dark2">Perpustakaan Gasibu</p>
+									
+									<div class="container-e container1">
+										<object data="../assets/pencarian/container/container-icon.svg" class="container-icon" type="image/svg+xml"></object>
+										<p class="container-text5">4.3</p>
+									</div>
+									
+									<p class="card-place-text-paragraph1">Jl. Majapahit, Citarum, Bandung Wetan</p>
+									<button class="btn-margin card-place-btn-margin hover-dark">Colokan</button>
+								</div>
+							</a>
+							
+							<a href="detail.php" class="card-place4 card-white" style="text-decoration: none; color: inherit;">
+								<img src="../assets/pencarian/card-place-wire-box1.png" class="wire-box-b card-place-wire-box3" />
+								
+								<div class="card-place-container4">
+									<p class="text-dark2">CUPS Coffee & Kitchen</p>
+									
+									<div class="card-place-container5">
+										<object data="../assets/pencarian/container/container-icon.svg" class="card-place-icon" type="image/svg+xml"></object>
+										<p class="card-place-text">4.1</p>
+									</div>
+									
+									<p class="card-place-text-paragraph3">Jl. Trunojoyo No. 25, Citarum, Bandung Wetan</p>
+									<button class="btn-margin card-place-btn-margin2 hover-dark">Tenang</button>
+								</div>
+							</a>
+							
+							<a href="detail.php" class="card-place-b card-place5" style="text-decoration: none; color: inherit;">
+								<img src="../assets/pencarian/card-place/card-place-wire-box2.png" class="wire-box-b card-place-wire-box2" />
+								
+								<div class="card-place-container2">
+									<p class="text-dark2">Eduplex Coworking Space</p>
+									
+									<div class="container-e container2">
+										<object data="../assets/pencarian/container/container-icon.svg" class="container-icon" type="image/svg+xml"></object>
+										<p class="container-text5">4.6</p>
+									</div>
+									
+									<p class="card-place-text-paragraph2">Jl. Ir. H. Djuanda No. 84, Bandung</p>
+									
+									<div class="card-place-container3">
+										<button class="btn-tag card-place-btn-tag1 hover-dark">Tenang</button>
+										<button class="btn-tag card-place-btn-tag2 hover-dark">AC</button>
+									</div>
+								</div>
+							</a>
+							
+							<a href="detail.php" class="card-place6 card-white" style="text-decoration: none; color: inherit;">
+								<img src="../assets/pencarian/card-place-wire-box2.png" class="wire-box-b card-place-wire-box4" />
+								
+								<div class="card-place-container6">
+									<p class="text-dark2">Grind Joe Coffee</p>
+									
+									<div class="container-e container4">
+										<object data="../assets/pencarian/container/container-icon.svg" class="container-icon" type="image/svg+xml"></object>
+										<p class="container-text5">4.6</p>
+									</div>
+									
+									<p class="card-place-text-paragraph4">Jl. Ir. H. Juanda No. 69, Tamansari, Bandung</p>
+									
+									<div class="card-place-container7">
+										<button class="btn-tag card-place-btn-tag3 hover-dark">WiFi</button>
+										<button class="btn-tag card-place-btn-tag4 hover-dark">AC</button>
+										<button class="btn-tag card-place-btn-tag5 hover-dark">Colokan</button>
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
